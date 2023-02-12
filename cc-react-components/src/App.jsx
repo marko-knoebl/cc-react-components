93ef596817ec137dc27b4f2e6f6ed08cdb054039
Marko Knöbl <marko.kn@gmail.com>
2023-02-12 22:51:41 +0100

    accordion component

--- cc-react-components/src/App.jsx
+++ cc-react-components/src/App.jsx
@@ -1,66 +1,53 @@
-import { useState } from 'react'
-import './App.css'
-import reactLogo from './assets/react.svg'
-import AppBar from './components/AppBar'
+import { useState } from "react";
+import "./App.css";
+import reactLogo from "./assets/react.svg";
+import AppBar from "./components/AppBar";
 
-import Alert from './components/Alert'
-
+import Alert from "./components/Alert";
+import Accordion from "./components/Accordion";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-
-      
-      <div><AppBar /></div>
-
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <AppBar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card"> 
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className='alert-container' >
-      <Alert 
-      type={"error"}
-      title={"Error"}
-      text={"This is an error alert"}
-      />
-      <Alert 
-      type={"warning"}
-      title={"Warning"}
-      text={"This is a warning alert"}
-      />
-         <Alert 
-      type={"info"}
-      title={"Info"}
-      text={"This is an info alert"}
-      />
-         <Alert 
-      type={"success"}
-      title={"Success"}
-      text={"This is a success alert"}
-      />
-      </div>
- 
 
+      <h2>Accordion</h2>
+      <Accordion title="accordion item with configurable content">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+        Magnam eos fugiat quas eum mollitia blanditiis sunt earum, voluptas,
+        numquam quae harum repudiandae facilis?
+        <br />
+        Porro rerum eius earum vitae corrupti corporis?
+      </Accordion>
+      <Accordion title="another accordion item">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. <br />
+        Magnam eos fugiat quas eum mollitia blanditiis sunt earum, voluptas,
+        numquam quae harum repudiandae facilis?
+        <br />
+        Porro rerum eius earum vitae corrupti corporis?
+      </Accordion>
+
+      <h2>Alert</h2>
+      <div className="alert-container">
+        <Alert type={"error"} title={"Error"} text={"This is an error alert"} />
+        <Alert
+          type={"warning"}
+          title={"Warning"}
+          text={"This is a warning alert"}
+        />
+        <Alert type={"info"} title={"Info"} text={"This is an info alert"} />
+        <Alert
+          type={"success"}
+          title={"Success"}
+          text={"This is a success alert"}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
